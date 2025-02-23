74772a5c01d0a4fd93153e28d6c7645c082e4b0e
antoniomv4702 <anmovi4702@hotmail.com>
2025-02-23 21:29:18 +0100

    Prac02v1.2

--- Tema4-Practica/fechas.js
+++ Tema4-Practica/fechas.js
@@ -1,9 +1,12 @@
 function calculateDays() {
     const startDate = new Date(document.getElementById('startDate').value);
     const endDate = new Date(document.getElementById('endDate').value);
-
-    if (startDate && endDate) {
-        const timeDifference = endDate - startDate;
+    const actDate = new Date();
+    document.getElementById('fch_ini').innerText = `Fecha.ini: ${startDate}`;
+    document.getElementById('fch_fin').innerText = `Fecha.fin: ${endDate}`;
+    document.getElementById('fch_act').innerText = `Fecha.act: ${actDate}`;
+    if (startDate && actDate) {
+        const timeDifference = endDate - actDate;
         const dayDifference = timeDifference / (1000 * 3600 * 24);
         if (dayDifference >= 365) {
             const year = dayDifference / (365);
